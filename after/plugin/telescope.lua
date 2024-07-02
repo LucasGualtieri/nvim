4853d6a01504d6d1727bbbd18ec404448148b2f2
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-07-02 12:02:55 -0300

    telescope key bindings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,14 @@
+require('telescope').setup{
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-k>"] = require('telescope.actions').move_selection_previous, -- move up
+                ["<C-j>"] = require('telescope.actions').move_selection_next, -- move down
+            },
+        },
+    },
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
