48ed0486ee130c7bdb49f2e1c84c04bd141d1af7
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-11-06 01:15:34 -0300

    teste

---
+++ teste.java
@@ -0,0 +1,16 @@
+public class teste {
+
+	public static void main(String[] args) {
+
+		System.out.println("teste");
+	}
+
+	private int foo() {
+
+		if (true) {
+			int x = 1;
+		}
+
+		return 1;
+	}
+}
