a5b0453711e3121e9e71cb0b3d7a13cfd367cd50
Lucas Gualtieri <lucasgualtieri.dev@gmail.com>
2024-11-26 00:36:17 -0300

    .

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
+vim.opt.termguicolors = true -- This needed to be here because of the colorizer plugin
 require("config.lazy")
 require("custom")
---
+++ lua/plugins/minty.lua
@@ -0,0 +1,19 @@
+return {
+	{ "nvzone/volt" },
+	{ "nvzone/minty", cmd = { "Shades", "Huefy" } },
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function ()
+			-- Attaches to every FileType mode
+			-- require 'colorizer'.setup()
+
+			-- Attach to certain Filetypes, add special configuration for `html`
+			-- Use `background` for everything else.
+			require 'colorizer' .setup {
+				'css';
+				'javascript';
+				html = { mode = 'foreground'; }
+			}
+		end
+	}
+}
